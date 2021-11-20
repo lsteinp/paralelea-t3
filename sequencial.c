6b954f76144a1f592cb06ced5ce681545e6e881a
Stein <stein@tiquetaque.com>
2021-11-20 19:44:18 -0300

    array size

--- sequencial.c
+++ sequencial.c
@@ -3,7 +3,7 @@
 #include "mpi.h"
 
 #define DEBUG 1            // comentar esta linha quando for medir tempo
-#define ARRAY_SIZE 40      // trabalho final com o valores 10.000, 100.000, 1.000.000
+#define ARRAY_SIZE 100.000      // trabalho final com o valores 10.000, 100.000, 1.000.000
 
 void bs(int n, int * vetor)
 {
